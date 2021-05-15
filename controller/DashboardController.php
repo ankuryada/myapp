5e1dd73f673bcd463fa71edaddb824ea874cd43c
ankuryada <46905315+ankuryada@users.noreply.github.com>
2021-05-16 00:15:07 +0530

    Add files via upload

---
+++ controller/DashboardController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+
+class DashboardController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+
+        $uid = auth()->user()->id;
+
+        $posts = DB::select('select * from posts where user_id = "'.$uid .'"');
+
+
+
+        return view('dashboard')->with('posts',$posts);
+    }
+}
